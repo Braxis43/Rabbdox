3bac0d0c425b0f5e78cbeca030fe4b52012642c8
Joseph C. Price, PHO <rtosplicer1time@icloud.com>
2024-10-27 13:01:31 -0500

    Create Fix-1.py

---
+++ Fix-1.py
@@ -0,0 +1,111 @@
+
+(*)
+z + z = 33
+45z = 33
+z = 33/45
+z = 11/15
+(*)
+compile error. 45z = 33 is not true. 45z = 33z is true.
+z = 33/45
+z = 11/15
+(*)
+z + z = 33
+2z = 33
+z = 33/2
+z = 16.5
+(*)
+z + z = 33
+2z = 33
+z = 33/2
+z = 16.5
+(*)
+u + h = (1/2)u + (1/2)hrs
+u + h = (1/2)u + (1/2)hrs/2/zulu
+(*)0 = (1/2)u + (1/2)hrs/2/zulu - u - h
+0 = (1/2)u + (1/2)hrs/2/zulu - u - h
+00/00+1/2u = 1/2hrs/2zulu - u - h x 2zulu
+00/00+1/2u = 1/2hrs - 2zu - 2h
+00/00+1/2u = 1/2hrs - 2zu - 2h
+box.find 
+relate 
+(*)
+d x a = b + c = e
+placement.seek
+relate
+(*)
+yield = 1/2 x (
+(*)
+yield = 1/2 x (b + c)
+(*)
+yield = 1/2 x (b + c)
+(*)
+latter.??//apply
+(*)
+latter.??//apply
+(*)
+latter.??//apply
+(*)
+staticmethod.loop({1,2,3,4,5,6,7,8,9,10})
+(*)
+staticmethod.loop({1,2,3,4,5,6,7,8,9,10})
+(*)
+staticmethod.loop({1,2,3,4,5,6,7,8,9,10})
+zip.log.run-query
+{33.z}
+(*)
+zip.log.run-query
+{33.z}
+(*)
+zip.log.run-query
+{33.z}
+(*)
+AI-model. found 33.z//run
+venner.33.z
+00/00+1/2u = 1/2hrs - 2zu - 2h
+(*)
+AI-model. found 33.z//run
+venner.33.z
+/*/0/0/0/1/33/99/set
+operator.pow(33, 99)
+(*)
+operator.pow(33, 99)
+(*)
+recieve.async(33, 99)
+//0/0/0/1/33/99/set
+++33++{99}[breakpoint]
+stop 
+exit
+wait
+UnicodeEncodeError
+EOFError.killed 
+supreme.<<33>>.99.441.2'61'super
+1/2u = 1/2hrs - 2zu - 2h
+23/2u = 1/2hrs - 2zu - 2h
+78/2u = 1/2hrs - 2zu - 2h
+121/2u = 1/2hrs - 2zu - 2h
+(*)(*)=(*)(*)-(*)(*)-(*)(*)
+(*)(begin) = (*)(begin) - (*)(begin) - (*)(begin)
+(*)(order) = (*)(order) - (*)(order) - (*)(order)
+(*)(FileExistsError) = (*)(FileExistsError) - (*)(FileExistsError) - (*)(FileExistsError)
+RecursionError = RecursionError - RecursionError - RecursionError
+(*)(begin) = (*)(begin) - (*)(begin) - (*)(begin)
+(*)(order) = (*)(order) - (*)(order) - (*)(order)
+(*)(FileExistsError) = (*)(FileExistsError) - (*)(FileExistsError) - (*)(FileExistsError)
+RecursionError = RecursionError - RecursionError - RecursionError
+(*)(begin) = (*)(begin) - (*)(begin) - (*)(begin)
+(*)(order) = (*)(order) - (*)(order) - (*)(order)
+(*)(FileExistsError) = (*)(FileExistsError) - (*)(FileExistsError) - (*)(FileExistsError)
+RecursionError = RecursionError - RecursionError - RecursionError
+(*)(begin) = (*)(begin) - (*)(begin) - (*)(begin)
+(*)(order) = (*)(order) - (*)(order) - (*)(order)
+(*)(FileExistsError) = (*)(FileExistsError) - (*)(FileExistsError) - (*)(FileExistsError)
+RecursionError = RecursionError - RecursionError - RecursionError
+(*)(begin) = (*)(begin) - (*)(begin) - (*)(begin)
+(*)(order) = (*)(order) - (*)(order) - (*)(order)
+(*)(FileExistsError) = (*)(FileExistsError) - (*)(FileExistsError) - (*)(FileExistsError)
+RecursionError = RecursionError - RecursionError - RecursionError
+(*)(begin) = (*)(begin) - (*)(begin) - (*)(begin)
+(*)(order) = (*)(order) - (*)(order) - (*)(order)
+(*)(FileExistsError) = (*)(FileExistsError) - (*)(FileExistsError) - (*)(FileExistsError)
+End 
+exit 
